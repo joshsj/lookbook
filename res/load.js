9f637a8ee37c7ba02ea5256d7a0898cf2c1ba1ac
joshsj <33764106+joshsj@users.noreply.github.com>
2019-07-17 05:30:24 +0100

    Implement sources loading

---
+++ res/load.js
@@ -0,0 +1,20 @@
+fetch("./res/sources.json")
+  .then(response => response.json()) // get json data
+  .then(data => {
+    data = data[document.title]; // sources for current page
+
+    const container = document.querySelector("main"); // images container
+
+    // add images
+    data.forEach(e => {
+      const image = document.createElement("img");
+      image.src = e.img; // add attributes
+      image.alt = e.alt;
+
+      const obj = document.createElement("a");
+      obj.href = e.page;
+      obj.innerHTML = image.outerHTML; // add img to link
+
+      container.append(obj);
+    });
+  });
